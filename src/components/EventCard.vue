025b243eac63f895631f20a3597644cdb77d0d92
Caio Jacobina <caiosantanaj@gmail.com>
2024-01-05 00:41:38 +0000

    SFComponents

---
+++ src/components/EventCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <!-- Display the event data -->
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
